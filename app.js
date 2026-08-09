7f380ac78bb3298d4927a3795306dad553a9ce87
ashusky12 <immadhukarsarkar@gmail.com>
2026-08-09 23:12:07 +0530

    Customize rare items included section with glowing amber stars and clean tags

--- app.js
+++ app.js
@@ -808,7 +808,8 @@ function renderAccountDetails(slug) {
   let rareItemsHtml = "";
   listing.rareItems.forEach(item => {
     rareItemsHtml += `
-      <span class="chip chip-mythic" style="text-transform: none; font-size: 0.8125rem; font-weight: 500; padding: 0.375rem 0.75rem;">
+      <span class="rare-item-tag">
+        <i data-lucide="star"></i>
         ${item}
       </span>
     `;
